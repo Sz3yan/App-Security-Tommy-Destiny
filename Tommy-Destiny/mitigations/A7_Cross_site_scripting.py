af5689048d32e1e439c93a789ef7c598ef924562
YeePingCyber <67046533+YeePingCyber@users.noreply.github.com>
2022-07-24 01:18:06 +0800

    V0.0.25: fire is burning

--- Tommy-Destiny/mitigations/A7_Cross_site_scripting.py
+++ Tommy-Destiny/mitigations/A7_Cross_site_scripting.py
@@ -1,10 +1,11 @@
+from multiprocessing import set_forkserver_preload
 from flask_csp.csp import csp_header
-from static.py.firebaseConnection import FirebaseClass
+from static.py.firebaseConnection import FirebaseAdminClass
 # <meta http-equiv="Content-Security-Policy" content="default-src 'self'; img-src https://images.unsplash.com;">
 # @csp_header(CspClass().return_csp_header())
 class CspClass:
     def __init__(self):
-        self.__firebaseDb = FirebaseClass()
+        self.__fba_db = FirebaseAdminClass()
         self.__default_src = ""
         self.__script_src = ""
         self.__script_src_elem = ""
@@ -14,12 +15,44 @@ class CspClass:
         self.__style_src_elem = ""
         self.__style_src_attr = ""
 
-    def create_policy(self):
-        pass
+    def create_policy(self, child_name:str, default_src:list=None, script_src:list=None, script_src_elem:list=None, script_src_attr:list=None, img_src:list=None, style_src:list=None, style_src_elem:list=None, style_src_attr:list=None):
+        policy_dict = {}
+        
+        if default_src:
+            policy_dict['default-src'] = default_src
 
-    def return_csp_header(self):
-        return {'default-src':"'self'"}
+        if script_src:
+            policy_dict['script-src'] = script_src
 
+        if script_src_elem:
+            policy_dict['script-src-elem'] = script_src_elem
 
+        if script_src_attr:
+            policy_dict['script-src-attr'] = script_src_attr
+
+        if img_src:
+            policy_dict['img-src'] = img_src
+
+        if style_src:
+            policy_dict['style-src'] = style_src
+
+        if style_src_elem:
+            policy_dict['style-src-elem'] = style_src_elem
+
+        if style_src_attr:
+            policy_dict['style-src-attr'] = style_src_attr
+
+        self.__fba_db.fa_create_csp(child_name, policy_dict)
+
+    def return_csp_header(self, child_name:str):
+        db_policy = self.__fba_db.fa_get_csp()[child_name]
+
+        for key,value in db_policy.items():
+            db_policy[key] = value.joined(' ')
+
+        return db_policy
+
+if __name__ == "__main__":
+    print(CspClass().return_csp_header('homepage'))
 
     
