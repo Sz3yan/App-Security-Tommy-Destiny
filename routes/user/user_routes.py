5c2bf939925c9c95d1a4f10cebfdb3c78bdcc4fa
Sz3yan <sz3yan@gmail.com>
2022-08-16 20:46:07 +0800

    twillio secret secured

--- routes/user/user_routes.py
+++ routes/user/user_routes.py
@@ -1,51 +1,49 @@
 import json
 import random
 
-from datetime import datetime
-from twilio.rest import Client
-
 from flask import Blueprint, render_template, request, session, redirect, url_for
-from mitigations.A3_Sensitive_data_exposure import AES_GCM, GoogleCloudKeyManagement
-from mitigations.API10_Insufficient_logging_and_monitoring import GoogleCloudLogging
-from static.firebaseConnection import FirebaseClass, FirebaseAdminClass
+from flask_jwt_extended import create_access_token
+from static.firebaseConnection import FirebaseClass
 from routes.user.static.py.Forms import CreateUser, LoginUser, TwoFactorAuth, EnterOTP
 from routes.admin.static.py.Post import Post
+from mitigations.A3_Sensitive_data_exposure import AES_GCM, GoogleCloudKeyManagement, GoogleSecretManager
+from mitigations.API10_Insufficient_logging_and_monitoring import GoogleCloudLogging
+from twilio.rest import Client
+
 
 user = Blueprint('user', __name__, template_folder="templates", static_folder='static')
 
-#User_Logger = GoogleCloudLogging()
-
+write_logs = GoogleCloudLogging()
+googlesecretmanager = GoogleSecretManager()
 keymanagement = GoogleCloudKeyManagement()
-secret_key_post = str(keymanagement.retrieve_key("tommy-destiny", "global", "my-key-ring", "hsm_tommy"))
-secret_key_page = str(keymanagement.retrieve_key("tommy-destiny", "global", "my-key-ring", "hsm_tommy1"))
+
+secret_key_post = str(keymanagement.retrieve_key("tommy-destiny", "global", "my-key-ring", googlesecretmanager.get_secret_payload("tommy-destiny", "hsm_tommy", "1")))
+secret_key_page = str(keymanagement.retrieve_key("tommy-destiny", "global", "my-key-ring", googlesecretmanager.get_secret_payload("tommy-destiny", "hsm_tommy1", "1")))
 
 recapcha_public_key = googlesecretmanager.get_secret_payload("tommy-destiny", "recaptcha-public-key", "1")
-
+twilio_account_sid = googlesecretmanager.get_secret_payload("tommy-destiny", "twilio_account_sid", "1")
+twilio_auth_token = googlesecretmanager.get_secret_payload("tommy-destiny", "twilio_auth_token", "1")
 
 @user.route("/")
 def index():
     try:
         firebase = FirebaseClass()
         posts = [post.val() for post in firebase.get_post().each()]
-        #User_Logger.log_info("User home: retrieved posts")
+        write_logs.write_entry_info("User home: retrieved posts")
     except:
         posts = []
-        #User_Logger.log_exception("User home: no posts found")
+        write_logs.write_entry_exception("User home: no posts found")
 
     return render_template('home.html', posts=posts)
 
 
-@user.route("/pricing")
-def pricing():
-    return render_template("pricing.html")
-
-
 @user.route("/login", methods=["POST", "GET"])
 def login():
-    #User_Logger.log_info("User login: access login page")
+    write_logs.write_entry_info("User login: access login page")
 
     firebase = FirebaseClass()
     loginUser = LoginUser(request.form)
+
     if "userID" in session:
         return redirect(url_for('user.profile'))
     else:
@@ -57,39 +55,27 @@ def login():
             if not firebase.login_user(email, password):
                 userID = firebase.get_user()
                 session['userID'] = userID
-                #User_Logger.log_info("User login: login successful, session created")
+                write_logs.write_entry_info("User login: login successful, session created")
                 return redirect(url_for("user.profile"))
             else:
-                #User_Logger.log_warning("User login: login failed")
+                write_logs.write_entry_warning("User login: login failed")
                 return render_template('login.html', form=loginUser, message=str(firebase.login_user(email, password)))
 
     return render_template('login.html', form=loginUser, message="", recapcha_public_key=recapcha_public_key)
 
-
-@user.route('/logout')
-def logout():
-    # remove the username from the session if it is there
-    session.pop('userID', None)
-
-    #User_Logger.log_info("User logout: logout successful, session removed")
-    return redirect(url_for('user.index'))
-
 @user.route('/2FA', methods=["POST", "GET"])
 def twoFactorAuth():
-    #User_Logger.log_info("User login: access login page")
-
     twoFactorAuth = TwoFactorAuth(request.form)
     if request.method == "POST" and twoFactorAuth.validate():
         phno = twoFactorAuth.phno.data
 
     else:
-        #User_Logger.log_info("User logout: logout successful, session removed")
         return render_template('2FA.html', form=twoFactorAuth)
     return render_template('2FA.html', form=twoFactorAuth)
 
+
 @user.route('/enterOTP', methods=["POST", "GET"])
 def enterOTP():
-    #    User_Logger.log_info("User login: access login page")
     enterOTP = EnterOTP(request.form)
     if request.method == "POST" and enterOTP.validate():
        otp = enterOTP.otp.data
@@ -97,23 +83,30 @@ def enterOTP():
        num = getOTPTwilio(phno)
        
     else:
-        #User_Logger.log_info("User logout: logout successful, session removed")
         return render_template('enterOTP.html', form= enterOTP)
     return render_template('enterOTP.html', form= enterOTP)
 
+
 def getOTPTwilio(phno):
-        TWILIO_ACCOUNT_SID = 'ACa38acb2c03e6b35c1bd7ba00fbdcd1a2'
-        TWILIO_AUTH_TOKEN = '5cce5cb9b1b2aaea8db39e183ff24629'
-        client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-        otp = random.randrange(100000,999999)
-        body = 'Your OTP is ' + str(otp)
-        message = client.messages.create(from_='+16506681171', body=body, to=phno)
-    
-        if message.sid:
-            return True
-        else:
-            return False
-        #TWILIO_VERIFY_SERVICE_ID = 'VAe790e68f842cb1f0f9e2bcf1714c0e62'
+    client = Client(twilio_account_sid, twilio_auth_token)
+    otp = random.randrange(100000,999999)
+    body = 'Your OTP is ' + str(otp)
+    message = client.messages.create(from_='+16506681171', body=body, to=phno)
+
+    if message.sid:
+        return True
+    else:
+        return False
+    #TWILIO_VERIFY_SERVICE_ID = 'VAe790e68f842cb1f0f9e2bcf1714c0e62'
+
+
+@user.route('/logout')
+def logout():
+    # remove the username from the session if it is there
+    session.pop('userID', None)
+
+    write_logs.write_entry_info("User logout: logout successful, session removed")
+    return redirect(url_for('user.index'))
 
 
 @user.route('/profile')
@@ -134,7 +127,7 @@ def profile():
 
 @user.route("/signup", methods=["POST", "GET"])
 def signup():
-    #User_Logger.log_info("User signup: access signup page")
+    write_logs.write_entry_info("User signup: access signup page")
 
     firebase = FirebaseClass()
     createUser = CreateUser(request.form)
@@ -146,9 +139,10 @@ def signup():
 
         if not firebase.create_user(email, password):
             firebase.create_user_info(username, phno, "customer")
-            #User_Logger.log_info("User signup: signup successful, user created")
+            write_logs.write_entry_info("User signup: signup successful, user created")
+            return render_template('login.html')
         else:
-            #User_Logger.log_warning("User signup: signup failed")
+            write_logs.write_entry_warning("User signup: signup failed")
             return render_template('signup.html', form=createUser, message=str(firebase.create_user(email, password)))
     return render_template('signup.html', form=createUser)
 
@@ -176,14 +170,14 @@ def top4post(id):
                 date = i.val()["_Post__published_at"]
 
                 decrypted = aes_gcm.decrypt(secret_key_post, plaintext)
-                #User_Logger.log_info(f"User post: decrypted post {id} with hsm_tommy key")
+                write_logs.write_entry_info(f"User post: decrypted post {id} with hsm_tommy key")
 
                 to_json = json.loads(decrypted)
                 data = to_json["blocks"]
             else:
                 data = data
     except:
-        #User_Logger.log_exception("User post: No posts found")
+        write_logs.write_entry_exception("User post: No posts found")
         return redirect(url_for("user.index"))
 
     return render_template('top4post.html', id=id, data=data, title=title, date=date)
@@ -212,18 +206,23 @@ def post(id):
                 date = i.val()["_Post__published_at"]
 
                 decrypted = aes_gcm.decrypt(secret_key_post, plaintext)
-                #User_Logger.log_info(f"User post: decrypted post {id} with hsm_tommy key")
+                write_logs.write_entry_info(f"User post: decrypted post {id} with hsm_tommy key")
 
                 to_json = json.loads(decrypted)
                 data = to_json["blocks"]
             else:
                 data = data
     except:
-        #User_Logger.log_exception("User post: No posts found")
+        write_logs.write_entry_exception("User post: No posts found")
         return redirect(url_for("user.index"))
 
     return render_template('post.html', id=id, data=data, title=title, date=date)
 
+
+@user.route("/pricing")
+def pricing():
+    return render_template("pricing.html")
+    
 
 @user.route("/about")
 def about():
@@ -243,14 +242,14 @@ def about():
                 plaintext = i.val()["_Page__plaintext"]
 
                 decrypted = aes_gcm.decrypt(secret_key_page, plaintext)
-                #User_Logger.log_info(f"User post: decrypted About page with hsm_tommy1 key")
+                write_logs.write_entry_info(f"User post: decrypted About page with hsm_tommy1 key")
 
                 to_json = json.loads(decrypted)
                 data = to_json["blocks"]
             else:
                 data = data
     except:
-        #User_Logger.log_exception("User about: no page found")
+        write_logs.write_entry_exception("User about: no page found")
         return redirect(url_for("user.index"))
 
     return render_template("about.html", data=data)
@@ -261,10 +260,10 @@ def allposts():
     try:
         firebase = FirebaseClass()
         posts = [post.val() for post in firebase.get_post().each()]
-        #User_Logger.log_info("User allposts: retrieved posts")
+        write_logs.write_entry_info("User allposts: retrieved posts")
     except:
         posts = []
-        #User_Logger.log_exception("User allposts: no posts found")
+        write_logs.write_entry_exception("User allposts: no posts found")
 
     return render_template("allposts.html", posts=posts)
 
@@ -287,14 +286,14 @@ def policy():
                 plaintext = i.val()["_Page__plaintext"]
 
                 decrypted = aes_gcm.decrypt(secret_key_page, plaintext)
-                #User_Logger.log_info(f"User post: decrypted Privacy-Policy page with hsm_tommy1 key")
+                write_logs.write_entry_info(f"User post: decrypted Privacy-Policy page with hsm_tommy1 key")
 
                 to_json = json.loads(decrypted)
                 data = to_json["blocks"]
             else:
                 data = data
     except:
-        #User_Logger.log_exception("User about: no page found")
+        write_logs.write_entry_exception("User about: no page found")
         return redirect(url_for("user.index"))
 
     return render_template("policy.html", data=data)
